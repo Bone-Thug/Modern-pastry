c437f0ee0560f5e4354a29019d9c6b9859b383df
Mohamed Dib Haj Ali <Mohamed Dib Haj Ali>
2017-07-06 19:02:14 +0300

    product schema

--- models/product.js
+++ models/product.js
@@ -1,9 +1,23 @@
 var mongoose = require("mongoose");
 var Scehma = mongoose.Schema;
 
-var ProductSchema = new Scehma({
+var productSchema = new Scehma({
+    
+    productName: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    
 
-   
+
 })
 
 module.exports = mongoose.model("product", userSchema);
