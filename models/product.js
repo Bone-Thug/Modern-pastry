5e69ab4b656ba89e17e4c8ade205a48fff5b294b
Mohamed Dib Haj Ali <Mohamed Dib Haj Ali>
2017-07-06 19:38:41 +0300

    fixed merge conflict

--- models/product.js
+++ models/product.js
@@ -2,7 +2,6 @@ var mongoose = require("mongoose");
 var Scehma = mongoose.Schema;
 
 var productSchema = new Scehma({
-<<<<<<< HEAD
     
     productName: {
         type: String,
@@ -17,14 +16,7 @@ var productSchema = new Scehma({
         required: true
     },
     
-
-=======
->>>>>>> 68b930339708cbfe5914e4fa7d19f48a003fa57f
-
 })
 
-<<<<<<< HEAD
-module.exports = mongoose.model("product", userSchema);
-=======
 module.exports = mongoose.model("product", productSchema);
->>>>>>> 68b930339708cbfe5914e4fa7d19f48a003fa57f
+
