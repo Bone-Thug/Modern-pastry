969a9dd6ff91be6894dec3c04d1467b81378ad8a
Mohamed Dib Haj Ali <Mohamed Dib Haj Ali>
2017-07-13 17:46:06 +0300

    home front end

--- public/script.js
+++ public/script.js
@@ -1,25 +1,29 @@
 var app = angular.module("myApp", ["ngRoute"]);
 
-app.controller("mainCtrl", function ($scope, $http) {
-});
+app.controller("mainCtrl", function ($scope, $http) {});
 
 app.directive('myNav', function () {
     return {
         templateUrl: 'nav.html'
-
     }
 });
 
+app.directive('myBody', function () {
+            return {
+                templateUrl: 'body.html'
+            }
+            });
+
 app.config(function ($routeProvider) {
     $routeProvider
-        .when("/", {
-            controller: 'mainCtrl',
-            templateUrl: 'components/home/home.html'
+//        .when("/", {
+//            controller: 'mainCtrl',
+//            templateUrl: 'components/index.html'
+//        })
+        .when('/', {
+            controller: 'newsCtrl',
+            templateUrl: 'components/home.html'
         })
-        .when('/home', {
-            controller: 'homeCtrl',
-            templateUrl: 'components/home/home.html'
-    })
 
         .when('/about', {
             controller: 'aboutCtrl',
@@ -32,6 +36,14 @@ app.config(function ($routeProvider) {
         .when('/news', {
             controller: 'newsCtrl',
             templateUrl: 'components/news/news.html'
+        })
+            .when('/sign-up', {
+            controller: 'newsCtrl',
+            templateUrl: 'components/sign-up.html'
+        })
+    .when('/sign-in', {
+            controller: 'newsCtrl',
+            templateUrl: 'components/sign-in.html'
         })
         .otherwise({
             redirectTo: '/'
